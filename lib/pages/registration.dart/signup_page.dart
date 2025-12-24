7a9f35c5774196dc18d0c61c6edf9244495ad774
Abdulrahman Abusnena <130256608+AbdulrahmanAbusnena@users.noreply.github.com>
2025-12-24 20:02:44 +0200

    git commit

--- lib/pages/registration.dart/signup_page.dart
+++ lib/pages/registration.dart/signup_page.dart
@@ -1,8 +1,13 @@
 import 'package:flutter/material.dart';
 
-class MyWidget extends StatelessWidget {
-  const MyWidget({super.key});
+class SignupPage extends StatefulWidget {
+  const SignupPage({super.key});
 
+  @override
+  State<SignupPage> createState() => _SignupPageState();
+}
+
+class _SignupPageState extends State<SignupPage> {
   @override
   Widget build(BuildContext context) {
     return const Placeholder();
