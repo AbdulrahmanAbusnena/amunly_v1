415d562671bce623e13f2ea955e7ea75203f1e8b
Abdulrahman Abusnena <130256608+AbdulrahmanAbusnena@users.noreply.github.com>
2025-12-28 21:49:30 +0200

    Update authentication_bloc.dart

--- lib/blocs/authentication_bloc/authentication_bloc.dart
+++ lib/blocs/authentication_bloc/authentication_bloc.dart
@@ -1,6 +1,6 @@
 import 'dart:async';
 
-import 'package:bloc/bloc.dart';
+import 'package:bloc/bloc.dart'; 
 import 'package:equatable/equatable.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/cupertino.dart';
